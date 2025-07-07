df8c6f3e9830a7b207a0043114454143863600cc
AndreaMeroi <andreameroidublin@gmail.com>
2025-07-07 23:37:50 +0200

    first commit

---
+++ biglietto.js
@@ -0,0 +1,21 @@
+
+
+let distance = 1
+const priceKm = 0.21
+const price = (priceKm * distance)
+
+console.log(distance)
+console.log(priceKm)
+console.log(price)
+
+const minorDiscount = 0.80
+const over65Discount = 0.60
+
+console.log(minorDiscount)
+console.log(over65Discount)
+
+if ()
+
+
+
+
